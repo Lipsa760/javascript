3b110c06c6f74eceec19eecee21d81ef15b1e5e3
Lipsamayee Jena <91116187+Lipsa760@users.noreply.github.com>
2023-08-27 22:40:38 +0530

    Update index.js

--- Test-2/index.js
+++ Test-2/index.js
@@ -1,42 +1,75 @@
-var Color = [
-  "green",
-  "navyblue",
-  "cyan",
-  "black",
-  "Crimson",
-  "voilet",
-  "blue",
-  "pink",
-  "maroon",
-  "indigo",
-  "yellow",
-  "beige",
-  "purple",
-  "orange",
-  "aqua",
-  "lime",
-];
-var index = 0;
-const circle = document.getElementById("circle");
-const shape = document.querySelector(".shape");
-const Changecolor = document.querySelector("#color");
-const changeShape = document.querySelector("#changeShape");
-Changecolor.addEventListener("click", () => {
-  if (index === Color.length) {
-    index = 0;
+
+const canvas = document.getElementById("canvas");
+const deleteButton = document.getElementById("dltbtn");
+const undoButton = document.getElementById("undobtn");
+const context = canvas.getContext("2d");
+let drawing = false;
+let objects = [];
+let tempPath = null;
+
+canvas.addEventListener("mousedown", start);
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", stop);
+canvas.addEventListener("mouseout", stop);
+
+deleteButton.addEventListener("click", cleanBoard);
+undoButton.addEventListener("click", undoWork);
+
+function start(e) {
+  drawing = true;
+  tempPath = [
+    { x: e.clientX - canvas.offsetLeft, y: e.clientY - canvas.offsetTop },
+  ];
+}
+
+function draw(e) {
+  if (!drawing) return;
+  context.lineWidth = 3;
+  context.lineCap = "round";
+  context.strokeStyle = "black";
+
+  tempPath.push({
+    x: e.clientX - canvas.offsetLeft,
+    y: e.clientY - canvas.offsetTop,
+  });
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  reWrite();
+}
+
+function stop() {
+  if (!drawing) return;
+  drawing = false;
+  if (tempPath.length > 1) {
+    addObjectToCanvas(tempPath);
   }
-  circle.style.backgroundColor = Color[index];
-  index++;
-});
-var Tringle = false;
-changeShape.addEventListener("click", () => {
-  if (!Tringle) {
-    var Shape = document.querySelector(".shape");
-    Shape.className = "triangle";
-    Tringle = true;
-  } else {
-    var shape = document.querySelector(".triangle");
-    shape.className = "shape";
-    Tringle = false;
+  tempPath = null;
+}
+
+function cleanBoard() {
+  objects = [];
+  reWrite();
+}
+
+function undoWork() {
+  objects.pop();
+  reWrite();
+}
+
+function reWrite() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (const obj of objects) {
+    context.beginPath();
+    context.moveTo(obj.path[0].x, obj.path[0].y);
+    for (const point of obj.path) {
+      context.lineTo(point.x, point.y);
+    }
+    context.stroke();
   }
-});
+}
+
+function addObjectToCanvas(path) {
+  objects.push({ path });
+  reWrite();
+}
