4376a247a543a1d8f93e93e4db0b079d6cf86aa6
Lipsamayee Jena <91116187+Lipsa760@users.noreply.github.com>
2023-08-27 22:33:03 +0530

    Add files via upload

---
+++ Test-2/index.js
@@ -0,0 +1,42 @@
+var Color = [
+  "green",
+  "navyblue",
+  "cyan",
+  "black",
+  "Crimson",
+  "voilet",
+  "blue",
+  "pink",
+  "maroon",
+  "indigo",
+  "yellow",
+  "beige",
+  "purple",
+  "orange",
+  "aqua",
+  "lime",
+];
+var index = 0;
+const circle = document.getElementById("circle");
+const shape = document.querySelector(".shape");
+const Changecolor = document.querySelector("#color");
+const changeShape = document.querySelector("#changeShape");
+Changecolor.addEventListener("click", () => {
+  if (index === Color.length) {
+    index = 0;
+  }
+  circle.style.backgroundColor = Color[index];
+  index++;
+});
+var Tringle = false;
+changeShape.addEventListener("click", () => {
+  if (!Tringle) {
+    var Shape = document.querySelector(".shape");
+    Shape.className = "triangle";
+    Tringle = true;
+  } else {
+    var shape = document.querySelector(".triangle");
+    shape.className = "shape";
+    Tringle = false;
+  }
+});
